4cf91480e12380555faa4b9785b96f74b9eef180
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-10-01 21:02:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,80 @@
 from flask import Flask, render_template, request, redirect, url_for
 from random import randint
+ 
 app = Flask(__name__)
 
 character_list = []
 
 class Character:
-    def __init__(self, name, character_class):
-        self.name = name
-        self.level = 0
-        self.exp = 0
-        self.character_class = character_class
-        if character_class == 'warrior':
-          self.classe = "Guerreiro"
-        elif character_class == 'thief':
-            self.classe = "Ladino"
-        else:
-            self.classe = "Mago"
-        self.strength = 10
-        self.intelligence = 10
-        self.dexterity = 10
-        self.generate_stats()
-        character_list.append((self.name, self.classe, self.strength, self.intelligence, self.dexterity))
+  def __init__(self, name):
+    self.name = name
+    self.level = 0
+    self.exp = 0
+    self.character_class = "Nenhuma"
+    self.set_base_stats()
+    self.add_to_character_list()
 
-    def get_exp(self):
-        self.exp += randint(1,100)
-        if self.exp >= 100:
-            self.level_up()        
-        return self.exp
-    
-    def generate_stats(self):
-        if self.character_class == "warrior":
-            self.strength += randint(2,5)
-            self.intelligence += randint(0,1)
-            self.dexterity += randint(0,3)
-        elif self.character_class == "wizard":
-            self.strength += randint(0,1)
-            self.intelligence += randint(2,5)
-            self.dexterity += randint(0,3)
-        else:
-            self.strength += randint(0,1)
-            self.intelligence += randint(0,3)
-            self.dexterity += randint(2,5)          
-        print(f"{self.name}, Class: {self.character_class}, Strength: {self.strength}; Intelligence: {self.intelligence}; Dexterity: {self.dexterity}")
-        return f"{self.name}, Class: {self.character_class}, Strength: {self.strength}; Intelligence: {self.intelligence}; Dexterity: {self.dexterity}"
+  def add_to_character_list(self):
+    character_list.append((self.name, self.character_class))
+    self.index = character_list.index((self.name, self.character_class));
+    character_list[(self.index)] = ((self.index, self.name, self.character_class, self.strength, self.intelligence, self.dexterity))
+    print(self.index)
 
-    def level_up(self):
-        print("Level Up!")
-        self.level += 1
-        self.generate_stats()
-        print(f"Current level: {self.level}")
-        return self.level
+  def set_base_stats(self):
+    self.strength = 10
+    self.intelligence = 10
+    self.dexterity = 10
+
+  def get_exp(self):
+    self.exp += randint(1,100)
+    if self.exp >= 100:
+      self.level_up()     
     
-    def debug(self):
-        while self.level < 5:
-            Character.get_exp(self)
-        print("Done testing.")
-        return True
+  def level_up(self):
+    self.level += 1
+    self.strength += randint(1,2)
+    self.intelligence += randint(1,2)
+    self.dexterity += randint(1,2)
+    print (f"Level Up! Current level: {self.level}")
+    
+  def debug(self):
+    while self.level < 5:
+      self.get_exp()
+    print(self.strength)
+    print(self.intelligence)
+    print(self.dexterity)
+    print("Done testing.")
+    print(f"Level: {self.level}, name: {self.name}, index: {self.index}")    
+
+class Wizard(Character):
+  def __init__(self, name):
+    super().__init__(name)
+  
+  def set_base_stats(self):
+    self.character_class = "Mago"
+    self.strength = 8 + randint(0,2)
+    self.intelligence = 12 + randint(0, 4)
+    self.dexterity = 10 + randint(0,2)
+
+class Warrior(Character):
+  def __init__(self, name):
+    super().__init__(name)
+  
+  def set_base_stats(self):
+    self.character_class = "Guerreiro"
+    self.strength = 12 + randint(0, 4)
+    self.intelligence = 8 + randint(0,2)
+    self.dexterity = 10 + randint(0,2)
+
+class Rogue(Character):
+  def __init__(self, name):
+    super().__init__(name)
+  
+  def set_base_stats(self):
+    self.character_class = "Ladino"
+    self.strength = 8 + randint(0,2)
+    self.intelligence = 10 + randint(0,2)
+    self.dexterity = 12 + randint(0, 4)
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
@@ -63,7 +82,14 @@ def hello():
   if request.method == 'POST':
     characterName = request.form['characterName']
     characterClass = request.form['characterClass']
-    character = Character(characterName, characterClass)
+    if characterClass == "warrior":
+      character = Warrior(characterName)
+    elif characterClass == "wizard":
+      character = Wizard(characterName)
+    elif characterClass == "rogue":
+      character = Rogue(characterName)
+    else:
+      character = Character(characterName, characterClass)
     if characterName != "":
       number_of_characters = len(character_list)
       return render_template('newcharacter.html', characterName=characterName, characterClass=characterClass, strength=character.strength, dexterity=character.dexterity, intelligence=character.intelligence)
